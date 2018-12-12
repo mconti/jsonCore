52b1158fbaad1727cda58c83fc28e42a5484b036
Maurizio <maurizio@MacBook-Pro-di-Xamarin.local>
2018-12-12 19:39:49 +0100

    Prima versione

---
+++ Models/studente.cs
@@ -0,0 +1,10 @@
+namespace PrimaCore.Controllers
+{
+    public class Studente
+    {
+        public string id{get;set;}
+        public string Nome{get;set;}
+        public string Cognome{get;set;}
+        
+    }
+}
